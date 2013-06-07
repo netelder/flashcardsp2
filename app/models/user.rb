1554facafac32935c83bb46361620640092563db
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-07 14:52:59 -0700

    updated routes and login logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
+
   has_many :rounds
   has_many :decks, :through => :rounds
 
   validates :email, :uniqueness => true
   validates :email, :password, :name, :presence => true
   # Remember to create a migration!
+
+  def check_password(password)
+    self.try(:authenticate, password)
+  end
+
 end
